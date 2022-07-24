4f6c735c4c8c6fef136bed896ea765e6aec06ef5
Vikas Sheoran <kkviks@gmail.com>
2022-07-24 22:53:19 +0530

    Add OneOf

--- golang/main.go
+++ golang/main.go
@@ -9,6 +9,19 @@ func main() {
 	fmt.Println(doSimple())
 	fmt.Println(doComplete())
 	fmt.Println(doEnum())
+	doOneOf(&pb.Result_Id{Id: 1})
+	doOneOf(&pb.Result_Message{Message: "huehuehue"})
+}
+
+func doOneOf(m interface{}) {
+	switch x := m.(type) {
+	case *pb.Result_Id:
+		fmt.Println(m.(*pb.Result_Id).Id)
+	case *pb.Result_Message:
+		fmt.Println(m.(*pb.Result_Message).Message)
+	default:
+		fmt.Printf("%v is neither a Result_Id nor a Message", x)
+	}
 }
 
 func doEnum() *pb.Enumeration {
